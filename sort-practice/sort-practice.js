4b4fe61746083bec13a0d79eb97e836ab2a387bd
Rodriguez <jrodriguez6@lm-sjc-11000598.corp.ebay.com>
2015-08-18 10:10:30 -0700

    removing cluttered logs

--- sort-practice/sort-practice.js
+++ sort-practice/sort-practice.js
@@ -57,45 +57,27 @@ var jsonParams = [
 var betterSort = function(data, params) {
 	data.sort(function(a, b) {
 		var compareResult = 0;
-
-		var debug = false;
-		if (a.lastName === 'Fwick' || b.lastName === 'Fwick') {
-			debug = true;
-			console.log('\na, b\n%j\n%j\n', a, b);
-		}
-
 		for (var i=0; i < params.length; i++) {
 			// Perform ASCENDING LOGIC always, then flip result if order=desc
 			if (a[params[i].field] < b[params[i].field]) {
 				compareResult = -1; 
-				if (debug) { console.log('%s, %s (%d) < %s, %s (%d): %s: %s', a.lastName, a.firstName, a.age, b.lastName, b.firstName, b.age, params[i].field, compareResult); }
-
 				if (params[i].order && params[i].order === 'desc') { // DESCENDING LOGIC
-					if (debug) { console.log('DESCENDING LOGIC'); }
 					compareResult = -compareResult; // Reverse compare result if DESC
 				}
 				break; // break out of sorting params if compareResult is already found
 			} else if (a[params[i].field] > b[params[i].field]) {
 				compareResult = 1;
-				if (debug) { console.log('%s, %s (%d) > %s, %s (%d): %s: %s', a.lastName, a.firstName, a.age, b.lastName, b.firstName, b.age, params[i].field, compareResult); }
-
 				if (params[i].order && params[i].order === 'desc') { // DESCENDING LOGIC
-					if (debug) { console.log('DESCENDING LOGIC'); }
 					compareResult = -compareResult; // Reverse compare result if DESC
 				}
 				break; // break out of sorting params if compareResult is already found
 			} else {
 				// via the current sorting param, a and b are equal, so move on
 				//continue;
-				if (debug) { console.log('%s, %s (%d) === %s, %s (%d): %s: %s', a.lastName, a.firstName, a.age, b.lastName, b.firstName, b.age, params[i].field, compareResult); }
 			}
 			
 			
 
-		}
-
-		if (debug) {
-			console.log('%s, %s (%d), %s, %s (%d): %d', a.lastName, a.firstName, a.age, b.lastName, b.firstName, b.age, compareResult);
 		}
 
 		return compareResult;
