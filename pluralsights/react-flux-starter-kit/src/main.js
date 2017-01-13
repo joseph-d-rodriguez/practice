8b3c8373187f97966e83fb26c8ac4aebbcf0a23e
Joseph Rodriguez <josephr@slalom.com>
2017-01-12 17:30:15 -0800

    simple react components with manual navigation and weird blank startup that works if you manually refresh page

--- pluralsights/react-flux-starter-kit/src/main.js
+++ pluralsights/react-flux-starter-kit/src/main.js
@@ -1,5 +1,37 @@
 $ = jQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
+var Header = require('./components/common/header');
 
-var App = console.log('Hello world from Browserify');
+// tutorial is having us do a substring(1) which is why i think i have to explicitely add this extra # character or else localhost:PORT won't load anything... must be localhost:PORT/#
+window.location = '/#'; 
 
-module.exports = App;
+(function(win) {
+	var App = React.createClass({
+		render: function() {
+			var Child;
+
+			switch(this.props.route) {
+				case 'about': Child = About; break;
+				default: Child = Home;
+			}
+
+			return (
+				<div>
+					<p>wtf</p>
+					<Header/>
+					<Child/>
+				</div>
+			);
+		}
+	});
+
+	function render() {
+		var route = win.location.hash.substr(1);
+		React.render(<App route={route} />, document.getElementById('app'));
+	}
+
+	win.addEventListener('hashchange', render);
+
+})(window);
