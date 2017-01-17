1637e1609b43560373857f0be9304b860316e9cc
Joseph Rodriguez <josephr@slalom.com>
2017-01-17 13:03:21 -0800

    async status and error handling implemented

--- pluralsights/pluralsight-redux-starter/src/components/home/HomePage.js
+++ pluralsights/pluralsight-redux-starter/src/components/home/HomePage.js
@@ -8,6 +8,7 @@ class HomePage extends React.Component {
 				<h1>Pluralsight Administrator</h1>
 				<p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
 				<Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+
 			</div>
 		);
 	}
--- pluralsights/pluralsight-redux-starter/src/reducers/index.js
+++ pluralsights/pluralsight-redux-starter/src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
 import authors from './authorReducer';
+import ajaxCallsInProgress from './ajaxStatusReducer';
 
 const rootReducer = combineReducers({
 	courses, // es6 terse shorthand property name left-hand only
-	authors
+	authors,
+	ajaxCallsInProgress
 });
 
 export default rootReducer;
